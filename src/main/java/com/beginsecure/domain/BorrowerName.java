f56071a5f772eb96a92abe5c35d478f0614fe0ff
mweaver20 <83778978+mweaver20@users.noreply.github.com>
2023-03-02 01:44:09 -0800

    testing new config

--- src/main/java/com/beginsecure/domain/BorrowerName.java
+++ src/main/java/com/beginsecure/domain/BorrowerName.java
@@ -1,2 +1,29 @@
-package com.beginsecure.domain;public class BorrowerName {
+package com.beginsecure.domain;
+
+public class BorrowerName {
+    private String firstName;
+    private String lastName;
+
+    public BorrowerName() {
+    }
+
+    public String getFullName(){
+        return firstName.toUpperCase() + " " + lastName.toUpperCase();
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
 }
--- src/test/java/com/beginsecure/domain/BorrowerNameTest.java
+++ src/test/java/com/beginsecure/domain/BorrowerNameTest.java
@@ -1,4 +1,33 @@
+package com.beginsecure.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BorrowerNameTest {
-  
+    private BorrowerName borrowerName;
+    @BeforeEach
+    void setUp() {
+        borrowerName = new BorrowerName();
+    }
+
+    @Test
+    void getFullName() {
+        borrowerName.setFirstName("lisa");
+        borrowerName.setLastName("simpson");
+        assertEquals("LISA SIMPSON", borrowerName.getFullName());
+    }
+
+    @Test
+    void getLastName() {
+        borrowerName.setLastName("simpson");
+        assertEquals("simpson", borrowerName.getLastName());
+    }
+
+    @Test
+    void getFirstName() {
+        borrowerName.setFirstName("bart");
+        assertEquals("bart", borrowerName.getFirstName());
+    }
 }
